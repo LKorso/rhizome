38a9e9ecc3d5347653446a589f4e8885fcf11461
LKorso <skrypnykdr@gmail.com>
2017-12-06 00:21:11 +0200

    Refactor - move validation logic to one js file

--- src/main/resources/public/js/registration.js
+++ src/main/resources/public/js/registration.js
@@ -3,7 +3,7 @@ $("document").ready(function () {
 });
 
 function onClick() {
-    cleanPage();
+    cleanValidation();
     register();
 }
 
@@ -13,18 +13,9 @@ function register() {
     }).fail(function (response) {
         if(response.status === 400) {
             $("#confirmPassword").val('');
-            response.responseJSON.errors.forEach(handleValidation);
+            showErrors(response.responseJSON.errors);
         }
     });
-}
-
-function handleValidation(error) {
-    if (error.fields.length === 1) {
-        applyErrorSpan("." + error.fields[0], error.message);
-    } else {
-        appendSpanForFewFields(error.fields, error.message);
-    }
-    higlightInputs(error.fields);
 }
 
 function createRegistrationDto() {
@@ -33,31 +24,4 @@ function createRegistrationDto() {
         "password"  : $("#password").val(),
         "confirmationPassword" : $("#confirmPassword").val()
     }
-}
-
-function cleanPage() {
-    $(".error-message-span").remove();
-    $("input").removeClass("wrong-input");
-}
-
-function defineFewFieldsSelector(fields) {
-    var selector = ".fewFieldsArea:has(";
-    fields.forEach(function (field) {
-        selector += "." + field + ", ";
-    });
-    return selector.substr(0, selector.length - 2) + ")";
-}
-
-function appendSpanForFewFields(fields, message) {
-    $(defineFewFieldsSelector(fields)).append("<span class='error-message-span'>" + message + "</span>");
-}
-
-function applyErrorSpan(selector, message) {
-    $("<span class='error-message-span'>" + message + "</span>").appendTo(selector);
-}
-
-function higlightInputs(fields) {
-    fields.forEach(function (field) {
-        $("." + field).children("input").addClass("wrong-input");
-    });
 }
