dbd09ee00791b3fcde07be681f1cd4f9802344c3
Dmytro Skrypnyk <dmytro_skrypnyk@epam.com>
2017-12-18 18:20:26 +0200

    [BE] Added OAuth2 security configuration

--- src/main/java/com/rhizome/web/security/configuration/SecurityConfiguration.java
+++ src/main/java/com/rhizome/web/security/configuration/SecurityConfiguration.java
@@ -2,24 +2,27 @@ package com.rhizome.web.security.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 import com.rhizome.web.security.AuthenticationExceptionsHandler;
 
-@EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@Configuration
+@EnableResourceServer
+public class SecurityConfiguration extends ResourceServerConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http
                 .authorizeRequests()
