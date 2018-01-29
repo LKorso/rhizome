da648db6e88b975e33cb1b7f6d4ecd5a3ff112ce
Dmytro Skrypnyk <dmytro_skrypnyk@epam.com>
2018-01-29 16:42:35 +0200

    Fixed error: 'Unsupported grant type: password'

--- src/main/java/com/rhizome/web/security/configuration/AuthorizationConfiguration.java
+++ src/main/java/com/rhizome/web/security/configuration/AuthorizationConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -39,12 +40,16 @@ public class AuthorizationConfiguration extends AuthorizationServerConfigurerAda
     @Autowired
     private ElasticClientsDetailsService clientsDetailsService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
                 .tokenStore(tokenStore())
                 .accessTokenConverter(accessTokenConverter())
-                .approvalStore(approvalStore);
+                .approvalStore(approvalStore)
+                .authenticationManager(authenticationManager);
     }
 
     @Override
