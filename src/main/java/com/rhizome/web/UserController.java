a3c1c3b0fa855c17d5cbdce47efd78927588cb33
Dmytro Skrypnyk <dmytro_skrypnyk@epam.com>
2018-03-20 14:38:24 +0200

    Ficed problem with accessing spring-data-rest endpoints

--- src/main/java/com/rhizome/web/UserController.java
+++ src/main/java/com/rhizome/web/UserController.java
@@ -5,29 +5,27 @@ import static org.springframework.http.HttpStatus.NO_CONTENT;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.rhizome.services.api.dto.UserData;
 import com.rhizome.services.implementation.UserService;
 
-@RestController
-@RequestMapping("/users")
+@RepositoryRestController
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/current")
+    @GetMapping("/users/current")
     public ResponseEntity currentUserProfile(Principal principal) {
         return ResponseEntity.ok(userService.findByEmail(principal.getName()));
     }
 
-    @PutMapping
+    @PutMapping("/users")
     @ResponseStatus(NO_CONTENT)
     public void updateUser(UserData userData, Principal principal) {
         userService.update(principal.getName(), userData);
